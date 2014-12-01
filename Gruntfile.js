43d24edc5dcc05c5645aea03633d7a8a9a9b8da2
=liqianjing <=jingqian_xi@163.com>
2014-12-01 13:59:46 +0800

    add config file

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+
+// 包装函数
+module.exports = function(grunt) {
+
+    // 任务配置,所有插件的配置信息
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        //以下配置用于工具，模块和逻辑层分离的情况
+        concat: {
+            basic_and_extras: {
+                files: {
+                    //新的文件的路径以及名称 : [有所的文件的路径名称]
+                    'src/conanjs.base/0.0.1/conanjs.base.min.js': ['src/conanjs.base/0.0.1/conanjs.base.js'],
+                    'src/conanjs.tips/0.0.1/conanjs.tips.min.js': ['src/conanjs.tips/0.0.1/conanjs.tips.js']
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'src/conanjs.base/0.0.1/conanjs.base.min.js': ['src/conanjs.base/0.0.1/conanjs.base.js'],
+                    'src/conanjs.tips/0.0.1/conanjs.tips.min.js': ['src/conanjs.tips/0.0.1/conanjs.tips.js']
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'example/conanjs.base/0.0.1/conanjs.base.min.css' : ['example/conanjs.base/0.0.1/conanjs.base.css'],
+                    'example/conanjs.tips/0.0.1/conanjs.tips.min.css' : ['example/conanjs.tips/0.0.1/conanjs.tips.css']
+                }
+            }
+        }
+    });
+
+    // 告诉grunt我们将使用插件
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // 告诉grunt当我们在终端中输入grunt时需要做些什么
+    grunt.registerTask('default', ['concat', 'uglify','cssmin']);
+
+};
