0384dd0b2c680d5a92a468aa8e8f0c4356469396
=liqianjing <=jingqian_xi@163.com>
2014-12-18 15:38:15 +0800

    slider修改，目前支持三种布局方式

--- src/conanjs.slider/0.0.1/conanjs.slider.js
+++ src/conanjs.slider/0.0.1/conanjs.slider.js
@@ -17,24 +17,17 @@
          */
         imgArr : [],
         /**
-         *  切换的方式,默认的是滚动
+         *  切换的方式,默认的是fade (支持三种方式的动画 animate-top : 上下滚动， animate-left : 左右滚动， animate-fade : 渐隐渐显)
          *
          *  @type   {String}
          */
-        animate : 'default',
-        /**
-         *  切换的方向(滚动支持上下和左右两种方式)
-         *  如果需要上下滚动 ： top ; 如果需要左右滚动 ： left
-         *
-         *  @type   {String}
-         */
-        path : 'top',
+        animate : 'animate-fade',
         /**
          *  动画的间隔时间
          *
          *  @type   {Number}
          */
-        speed : 1000
+        speed : 2000
     };
 
     function slider(option) {
@@ -65,7 +58,8 @@
                 targetWidth = null,
                 targetHeight = null,
                 uls = null,
-                html = null;
+                html = null,
+                animateParam = param.animate;
 
             for(var i = 0; i < len; i++){
                 lis.push('<li><img src="'+ imgs[i] +'" /></li>');
@@ -75,32 +69,38 @@
             html = '<div class="slider-wrap"><ul class="slider">'+ lis.join('') +'</ul></div><ol class="number">'+ number.join('') +'</ol>';
 
             target.innerHTML = html;
+            //设置默认选中
+            $D.addClass($D.tagName('li',$D.className('number')[0])[0],'on');
+
 
             //根据动画改变css，通过不同的class控制
             //如果是纵向滚动--coanajs-y
             //如果是横向滚动--conanjs-x
             //如果是渐隐渐显--coanajs-fade
-            if($T.isString(param.animate)) {
+            if($T.isString(animateParam)) {
                 uls = target.childNodes[0].childNodes[0];
-                if(param.animate === 'default' && $T.isString(param.path)) { //如果是要左右滚动
-                    if(param.path === 'top'){ //如果是要上下滚动
-                        $D.addClass(target,'conanjs-y');
-                        //得到外框的高度
-                        targetHeight = getDefaultStyle(target,'height');
-                        //动态的设置高度
-                        uls.style.height = (len * parseInt(targetHeight)) + 'px';
-                    }else { //如果要左右滚动
-                        $D.addClass(target,'conanjs-x');
-                        //得到外框的宽度
-                        targetWidth = getDefaultStyle(target,'width');
-                        //动态的设置宽度
-                        uls.style.width = (len * parseInt(targetWidth)) + 'px';
-                    }
-                }else if(param.animate === 'fade') { //如果是渐隐渐显
+                if(animateParam === 'animate-top') { //如果是要左右滚动
+                    $D.addClass(target,'conanjs-top');
+                    //得到外框的高度
+                    targetHeight = getDefaultStyle(target,'height');
+                    uls.style.height = (len * parseInt(targetHeight)) + 'px';
+                    animate('top');
+                }else if(animateParam === 'animate-left') { //如果是渐隐渐显
+                    $D.addClass(target,'conanjs-left');
+                    //得到外框的宽度
+                    targetWidth = getDefaultStyle(target,'width');
+                    //动态的设置宽度
+                    uls.style.width = (len * parseInt(targetWidth)) + 'px';
+
+                    animate('left');
+                }else { //如果参数有问题就直接把动画定义为fade
                     $D.addClass(target,'conanjs-fade');
+                    uls.style.width = getDefaultStyle(target,'width') + 'px';
+                    uls.style.height = getDefaultStyle(target,'height') + 'px';
+
+                    animate('fade',param.speed);
                 }
             }
-            animate(param.path);
         }
     }
     //元素的索引值
@@ -112,25 +112,45 @@
         }
     }
     //动画
-    function animate(path){
+    function animate(path,speed){
         //点击下面的小方块出现对应的图片
         var numberLis = $D.tagName('li',$D.className('number')[0]),
             slider = $D.className('slider')[0],
             imageLis = $D.tagName('li',slider),
-            scroll = path === 'left' ? 'width' : 'height',
-            imgWidth = getDefaultStyle(imageLis[0],scroll);
+            speed = speed || 2000,
+            scroll = path === 'left' ? 'width' : ('top' ? 'height' : 'fade');
 
         $E.on(numberLis,'click',function(){
+
             //现在的this指向每一个li（有循环）
             var $this = this,
                 indexNum = index($this,numberLis);
+            //当前选中的加一个class显示为当前的
+            AM.each(numberLis,function(){
+                $D.removeClass(this,'on');
+            });
+            $D.addClass($this,'on');
             //绑定索引值
             $this.setAttribute('data-index',indexNum);
             for(var i = 0,len = imageLis.length; i < len; i++){
                 imageLis[i].setAttribute('data-index',index(imageLis[i],imageLis));
             }
 
-            slider.style[path] = -indexNum * parseInt(imgWidth) + 'px';
+            if(path === 'fade'){
+                var current = parseInt($this.getAttribute('data-index'),10);
+                AM.each(imageLis,function(){
+                    var opacity = getDefaultStyle(this,'opacity');
+                    if(opacity > 0) {
+                        this.style.opacity = 0;
+                    }
+                });
+
+                conanjs.fade.fadein(imageLis[current], 100, speed);
+
+            }else {
+                var imgWidth = getDefaultStyle(imageLis[0],scroll);
+                slider.style[path] = -indexNum * parseInt(imgWidth) + 'px';
+            }
 
         });
         //定义定时器自动滚动
